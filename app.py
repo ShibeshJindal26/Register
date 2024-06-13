b8ad6f34cda975b0319aed9e55e17f77e7076b4e
ShibeshJindal26 <142495253+ShibeshJindal26@users.noreply.github.com>
2024-06-13 11:28:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -156,4 +156,5 @@ def capture():
         return jsonify({'error': 'No image captured'}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = 5000
+    app.run(debug=True, host='0.0.0.0', port=port)
